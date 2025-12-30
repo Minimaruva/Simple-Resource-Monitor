f7ebd7f8079c9cd7b6666d650beb9461e027b615
Vlad Y <vladjagnion@gmail.com>
2025-12-30 15:34:17 +0000

    basic prints for CPU

--- src/monitor.py
+++ src/monitor.py
@@ -4,22 +4,28 @@ from datetime import datetime
 
 
 def cpu_percent():
-    return psutil.cpu_percent(interval=1)
+    return psutil.cpu_percent()
 
 
 def clear_screen():
     print("\033[H\033[J", end="") # ANSI escape code to clear screen
 
 def pretty_print():
+    cpu_p = cpu_percent()
+    cpu_logical = psutil.cpu_count()
+    
+    clear_screen()
     print('='*50 + '\n='+' '*16+'Resource Monitor' + ' '*16+ '=\n'+'='*50)
-    print(psutil.users())
-    print(cpu_percent())
-    print(psutil.virtual_memory())
+    print(f"={' '*12}Time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}{' '*11}=")
+    print('='*50+'\n'+'='*50)
+    print(f"= CPU Usage: {cpu_p}% {' '*(7-len(str(cpu_p)))}|| CPU Cores (Logical): {cpu_logical} =")
+    print(f"={'-'*48}=")
+
 
     
 if __name__ == '__main__':
     while True:
         pretty_print()
         time.sleep(1)
-        clear_screen()
+        
 
